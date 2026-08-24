dc9613881bc282fbefb663a79b0242100c98df56
chayma <chayma.abdessadak@student.ehb.be>
2026-08-24 14:25:57 +0200

    Bedrijf klasse aangemaakt met fields

--- src/Bedrijf.java
+++ src/Bedrijf.java
@@ -1,2 +1,9 @@
 public class Bedrijf {
+    private String naam;
+    private String contactPersoon;
+    private String email;
+    private String telefoon;
+    private String adres;
+    private String omschrijving;
+    private String zoekterm;
 }
