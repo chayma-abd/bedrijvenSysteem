96f5e85d9657263b95f10f28ac159e0adf095c8a
chayma <chayma.abdessadak@student.ehb.be>
2026-08-24 14:36:27 +0200

    constructor toegevoegd aan Bedrijf

--- src/Bedrijf.java
+++ src/Bedrijf.java
@@ -6,4 +6,16 @@ public class Bedrijf {
     private String adres;
     private String omschrijving;
     private String zoekterm;
+
+    // Constructor
+    public Bedrijf(String naam, String contactPersoon, String email, String telefoon,
+                   String adres, String omschrijving, String zoekterm) {
+        this.naam = naam;
+        this.contactPersoon = contactPersoon;
+        this.email = email;
+        this.telefoon = telefoon;
+        this.adres = adres;
+        this.omschrijving = omschrijving;
+        this.zoekterm = zoekterm;
+    }
 }
