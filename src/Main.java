d16e87f5655757b3ac832bd45f0940f43e275548
chayma <chayma.abdessadak@student.ehb.be>
2026-08-25 09:51:34 +0200

    testdata toegevoegd met 5 bedrijven in Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        // Lijst met alle bedrijven
+        ArrayList<Bedrijf> bedrijven = new ArrayList<>();
+
+        // Testdata toevoegen - 5 bedrijven
+        bedrijven.add(new Bedrijf("TechNova", "Jan Peeters", "jan@technova.be", "0495123456",
+                "Brusselsesteenweg 100, Brussel", "Cybersecurity en IT-oplossingen", "stagebedrijf"));
+        bedrijven.add(new Bedrijf("GreenEnergy", "Marie Dupont", "marie@greenenergy.eu", "0478987654",
+                "Groenstraat 25, Leuven", "Duurzame energie en zonnepanelen", "sponsor"));
+        bedrijven.add(new Bedrijf("CodeFactory", "Tom De Smet", "tom@codefactory.com", "0499123456",
+                "Programmeerlaan 8, Gent", "Software ontwikkeling en AI", "stagebedrijf, gastles"));
+        bedrijven.add(new Bedrijf("HealthPlus", "Sofie Claes", "sofie@healthplus.be", "0478567890",
+                "Gezondheidsplein 42, Antwerpen", "Medische software en zorgtechnologie", "stagebedrijf"));
+        bedrijven.add(new Bedrijf("FinanceWise", "Wouter Maes", "wouter@financewise.be", "0498345678",
+                "Bankstraat 12, Brussel", "Financiële diensten en boekhouding", "sponsor, gastles"));
+
+        System.out.println("=== Welkom bij het KET Bedrijven Systeem ===");
+        System.out.println("Testdata geladen: " + bedrijven.size() + " bedrijven.");
     }
 }
